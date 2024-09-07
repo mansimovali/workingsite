f91582e1cd05646580de6695c0a44cffa1f57f32
mansimovali <alimansimovpc@gmail.com>
2024-09-08 00:15:27 +0400

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT =BASE_DIR/'media'
+MEDIA_URL = 'media/'
+# STATIC_ROOT = '/home/mansimovali/mysite/static'
+STATIC_ROOT =BASE_DIR/'static'
+# STATIC_URL = '/static/'
